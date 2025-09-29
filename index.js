7a886bd93ac9b34fd8c999dc76c42678afd580e6
kmanish726084@gmail.com <kmanish726084@gmail.com>
2025-09-30 00:26:16 +0530

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose=require("mongoose");
 const path=require("path");
 const Chat=require("./models/chat.js");
 const methodOverride=require("method-override");
+const ExpressError=require("./ExpressError");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -21,67 +22,95 @@ main()
     });
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+    await mongoose.connect("mongodb://127.0.0.1:27017/fakedata");
 };
 
 //<----------------index route--------------------------->
 app.get("/chats", async(req,res)=>{
     let chats= await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs",{chats});
 });
 
 //<----------------new route--------------------------->
 app.get("/chats/new",(req,res)=>{
+    // throw new ExpressError(404,"some error");
     res.render("new.ejs");
 });
 //<------------------create route--------------------------->
-app.post("/chats",(req,res)=>{
-    let {from, msg , to}=req.body;
-    let newChat= new Chat({
+app.post("/chats",async (req,res,next)=>{
+    try{
+        let {from, msg , to}=req.body;
+        let newChat= new Chat({
         from:from,
         to:to,
         msg:msg,
         created_at:new Date()
     });
-    // console.log(newChat);
-    newChat
-        .save()
-        .then((res)=>{
-            console.log("chat was saved");
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
-    res.redirect("/chats");
+    await newChat.save();
+        res.redirect("/chats");
+    }
+    catch(err){
+        next(err);
+    }
+ 
+});
+
+//<---------Show Route------------>
+app.get("/chats/:id", async(req,res,next)=>{
+    try{
+        let {id} = req.params;
+        let chat= await Chat.findById(id);
+        if(!chat){
+            next(new ExpressError(404,"Page not Found"));
+        }
+        // console.log(chat);
+        res.render("edit.ejs",{chat});
+    }
+    catch(err){
+        next(err);
+    }
 });
 
 //<---------Edit Route------------>
 app.get("/chats/:id/edit", async(req,res)=>{
-    let {id} = req.params;
-    let chat= await Chat.findById(id);
-    console.log(chat);
-    res.render("edit.ejs",{chat});
+    try {
+        let {id} = req.params;
+        let chat= await Chat.findById(id);
+        console.log(chat);
+        res.render("edit.ejs",{chat});
+    } catch (error) {
+        next(err);
+    }
 });
 
 // <-------------Update----------->
 app.put("/chats/:id", async (req,res)=>{
-    let {id}=req.params;
-    let {msg:newmsg}=req.body;
-    console.log(newmsg);
-    let updateChat= await Chat.findByIdAndUpdate(
-        id,{msg:newmsg},{runValidators:true, new:true}
-    );
-    console.log(updateChat);
-    res.redirect("/chats");
+    try{
+        let {id}=req.params;
+        let {msg:newmsg}=req.body;
+        console.log(newmsg);
+        let updateChat= await Chat.findByIdAndUpdate(
+            id,{msg:newmsg},{runValidators:true, new:true}
+        );
+        console.log(updateChat);
+        res.redirect("/chats");
+    }
+    catch(err){
+        next(err);
+    }
 });
 // <------------Delete Route-------------->
 app.delete("/chats/:id", async(req,res)=>{
-    let {id}=req.params;
-    let deleteChat=await Chat.findByIdAndDelete(id);
-    console.log(deleteChat);
-    res.redirect("/chats");
-})
+    try {
+         let {id}=req.params;
+        let deleteChat=await Chat.findByIdAndDelete(id);
+        console.log(deleteChat);
+        res.redirect("/chats");
+    } catch (error) {
+        next(err);
+    }
+});
 
 // let chat1=new Chat({
 //     from:"neha",
@@ -96,7 +125,12 @@ app.delete("/chats/:id", async(req,res)=>{
 app.get("/",(req,res)=>{
     res.send("root is working");
 });
+app.use((err,req,res,next)=>{
+    console.log(err);
+    let {status=500,message="Some Error Occurred"}=err;
+    res.status(status).send(message);
 
+})
 app.listen(8080,()=>{
     console.log("server is listenig on port 8080 ");
 });
